ae667d8b7844a3c0d4aabb83c519fcf403cb9bee
Leonardo Lemos <leonardo@mixd.com.br>
2018-01-18 23:39:32 -0200

    add some ui for user name filter: class 14

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,16 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        <li><a href="{{ route('threads.index') }}">All Threads</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" 
+                            role="button" aria-haspopup="true" aria-expanded="false">Threads <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="{{ route('threads.index') }}">All Threads</a></li>
+                                @if(auth()->check())
+                                <li><a href="{{ route('threads.index', [null, 'user' => auth()->user()->name]) }}">Threads by {{ auth()->user()->name }}</a></li>
+                                @endif
+                            </ul>
+                        </li>                        
                         <li><a href="{{ route('threads.create') }}">New Thread</a></li>
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" 
